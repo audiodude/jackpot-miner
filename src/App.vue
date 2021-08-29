3246e20351eec7d171e6b0647dcac96618c481f6
Travis Briggs <audiodude@gmail.com>
2021-08-28 22:47:23 -0700

    Remove template content

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div>Jackpot Miner</div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from "vue-class-component";
-import HelloWorld from "./components/HelloWorld.vue";
+import { Options, Vue } from 'vue-class-component';
+import HelloWorld from './components/HelloWorld.vue';
 
 @Options({
   components: {
@@ -15,13 +14,4 @@ import HelloWorld from "./components/HelloWorld.vue";
 export default class App extends Vue {}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
